29f2f389d247993e2cbc353691c23fed8a498d7f
AntoninThioux <antonin.thioux@gmail.com>
2023-04-08 10:43:30 +0200

    updated package version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="teeny-romatic-consequence",
-    version="0.0.2",
+    version="0.0.3",
     author="Antonin Thioux",
     author_email="antonin.thioux@gmail.com",
     description="testing pypi",
